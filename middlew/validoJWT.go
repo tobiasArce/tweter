552d3c69061fd47ae4d94e37cc98195b3fc328d8
lolito93 <tobias.f.arce@gmail.com>
2022-04-08 21:44:20 -0300

    handlers y jwt

--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -9,7 +9,7 @@ import (
 /*ValidoJWT permite validar el JWT que nos llega en la peticion*/
 func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authoritation"))
+		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
 		if err != nil {
 			http.Error(w, "Error en el Token!"+err.Error(), http.StatusBadRequest)
 			return
